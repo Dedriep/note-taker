a7855b61180dea9c892c5bc2ee8d9d2db5198e39
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-03-13 14:30:33 -0400

    2nd commit

--- server.js
+++ server.js
@@ -1,34 +1,101 @@
 const express = require('express');
 const app = express();
+
 const fs = require('fs');
 const path = require('path');
+
+const noteData = require('./db/db.json');
+const PORT = process.env.PORT || 3001;
+const ShortUniqueId = require('short-unique-id');
+const uid = new ShortUniqueId();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 
-const noteData = require('../db/db.json');
-const PORT = process.env.PORT || 3001;
+app.get('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (error, results) => {
 
-app.get('/api/index', (req, res) => {
-    let results = noteData;
-    if (req.query) {
-      results = filterfunction(req.query, results);
-    }
-    res.json(results);
-  });
+        //console.log("noteData", noteData);
+        //console.log("fs.readfile", results);
+
+        //Parse results to JSON object 
+        const allNotes = JSON.parse(results);
+        //console.log("fs.readfile", allNotes);
+
+        //ssend it back to HTML page 
+        res.json(allNotes);
+    });
+
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log("Id", uid());
+    //adding a new key/value 
+    const newNote = {...req.body, "id": uid()};
+    console.log("new note", req.body);
+    fs.readFile("./db/db.json", "utf8", (error, results) => {
+
+        //Parse results to JSON object 
+        const allNotes = JSON.parse(results);
+
+        //combine the old and new notes 
+        allNotes.push(newNote);
+        console.log(allNotes); 
+
+        fs.writeFile("./db/db.json", JSON.stringify(allNotes), (error, results) => {
+            if (error) {"error: ",  console.log(error)};
+
+            console.log("result", results);
+
+            res.json(allNotes);
+        });
+      
+    });
+   
+
+});
+//DELETE
+//fs.readfile
+//fs.writefile 
+//array.filter 
+
+app.delete('/api/notes', (req,res) => {
+
+    fs.readFile("./db/db.json", "utf8", (error, results) => {
+
+        
+
+
+        fs.writeFile("./db/db.json", JSON.stringify(noteData), (error, results) => {
+            if (error) {"error: ",  console.log(error)}
+            else { noteData.filter()
+
+            }
+
+            console.log("result", results);
+
+            res.json(allNotes);
+        });
+      
+    });
+    
+
+})
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
+});
 
-  app.get('*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+});
 
 
-app.listen(PORT , () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.listen(PORT, () => {
+    console.log(`API server now on port 3001! \n http://localhost:${PORT}`);
+});
+
+
 
